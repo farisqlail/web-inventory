650384c7056cfe2c5de1b0b520bcd1cc54b939ac
wedisDev <wedisdotco@gmail.com>
2022-07-08 19:13:40 +0700

    analisa rop & eoq

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -375,9 +375,14 @@ class HomeController extends Controller
     public function TampilDataBarangROP()
     {
         $data = DB::table('barang as ss')
+            ->select('ss.ID_BARANG', 'ss.NAMA_BARANG', 'st.NILAI_SS', 'ss.STOCK_BARANG', 'rp.NILAI_ROP', 'sp.NAMA_SUPPLIER')
             ->join('rop as rp', 'rp.ID_BARANG', '=', 'ss.ID_BARANG')
+            ->join('safety_stock as st', 'st.ID_BARANG', '=', 'ss.ID_BARANG')
+            ->join('supplier as sp', 'sp.ID_SUPPLIER', '=', 'ss.ID_SUPPLIER')
+            ->where('rp.STATUS_ROP', '=', '1')
+            ->groupBy('ss.ID_BARANG', 'ss.NAMA_BARANG', 'ss.STOCK_BARANG', 'rp.NILAI_ROP', 'st.NILAI_SS', 'sp.NAMA_SUPPLIER')
             ->get();
-
+        // dd($data);
         return View('gudang/operasibarang/dataBarangRop')
             ->with('DataBarangRop', $data);
     }
